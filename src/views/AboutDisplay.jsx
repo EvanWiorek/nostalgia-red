0f507c9bf5d8ec36367053f3f64bc79208f48e40
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-04-22 15:36:52 -0500

    update

--- src/views/AboutDisplay.jsx
+++ src/views/AboutDisplay.jsx
@@ -1,4 +1,6 @@
 import trainerCardBG from '../assets/trainer-card.png'
+import linkedIn from '../assets/linkedin.png'
+import github from '../assets/github.png'
 
 export default (props) => {
   return (
@@ -8,6 +10,8 @@ export default (props) => {
       <a href="http://evanwiorek.com" target='_blank' className='website-name'>EVANWIOREK.COM</a>
       <p className='links-text'>LINKS</p>
       <img src={trainerCardBG} alt="trainer card bg" className='trainer-card-bg'/>
+      <img src={linkedIn} alt="LinkedIn" className='linked-in' />
+      <img src={github} alt="github" className='github' />
     </div>
   )
 }
